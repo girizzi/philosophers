2999750fd7fd9e2520ac698c1d8c2306e6c49eac
Giancarlo Rizzi <girizzi@e3r8p7.42roma.it>
2025-11-07 15:46:54 +0100

    check_args completed

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: girizzi <girizzi@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 19:30:00 by girizzi           #+#    #+#             */
-/*   Updated: 2025/11/03 19:46:16 by girizzi          ###   ########.fr       */
+/*   Updated: 2025/11/07 15:03:34 by girizzi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,18 @@ int	check_isdigit(char *arg)
 
 int	check_args(char **argv)
 {
-	if (ft_atoi(argv[1]) > PHILO_MAX || ft_atoi(argv[1]) <= 0
-		|| check_arg_content(argv[1]) == 1)
-	if
+	if (ft_atoi(argv[1]) > PHILO_MAX || ft_atoi(argv[1]) <= 1
+		|| check_isdigit(argv[1]) == 1)
+		return (write(2, "Invalid number of philosophers\n", 31), 1);
+	if (ft_atoi(argv[2]) <= 0 || check_isdigit(argv[2]) == 1)
+		return (write(2, "Invalid time to die\n", 21), 1);
+	if (ft_atoi(argv[3]) <= 0 || check_isdigit(argv[3]) == 1)
+		return (write(2, "Invalid time to eat\n", 21), 1);
+	if (ft_atoi(argv[4]) <= 0 || check_isdigit(argv[4]) == 1)
+		return (write(2, "Invalid time to sleep\n", 23), 1);
+	if (argv[5] && (ft_atoi(argv[5]) < 0 || check_isdigit(argv[5]) == 1))
+		return (write(2, "Invalid number of lunches\n", 26), 1);
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -46,3 +55,13 @@ int	main(int argc, char **argv)
 	free_all();
 	return (0);
 }
+
+/*
+Usage: ./philo <N1> <N2> <N3> <N4> <N5*>
+
+N1: Number of philosophers (max 300)
+N2: Time to die
+N3: Time to eat
+N4: Time to sleep
+N5*optional: Number of lunches required
+*/
