d3a9710398f0466d2206e240006fd3c2531d9f99
G R <rzz.gcr@gmail.com>
2026-03-15 18:09:37 +0100

    fixed infinite wait

--- philo_bonus/init_bonus.c
+++ philo_bonus/init_bonus.c
@@ -6,7 +6,7 @@
 /*   By: girizzi <girizzi@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 11:15:00 by girizzi           #+#    #+#             */
-/*   Updated: 2026/03/15 17:58:01 by girizzi          ###   ########.fr       */
+/*   Updated: 2026/03/15 18:08:16 by girizzi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	init_semaphores(t_program *program)
 	program->forks = sem_open(SEM_FORKS, O_CREAT, 0644, program->n_philos);
 	program->write_lock = sem_open(SEM_WRITE, O_CREAT, 0644, 1);
 	program->meal_lock = sem_open(SEM_MEAL, O_CREAT, 0644, 1);
-	program->dead_lock = sem_open(SEM_DEAD, O_CREAT, 0644, 1);
+	program->dead_lock = sem_open(SEM_DEAD, O_CREAT, 0644, 0);
 	program->limit = sem_open(SEM_LIMIT, O_CREAT, 0644, program->n_philos / 2);
 	if (program->forks == SEM_FAILED || program->write_lock == SEM_FAILED
 		|| program->meal_lock == SEM_FAILED || program->dead_lock == SEM_FAILED
--- philo_bonus/main_bonus.c
+++ philo_bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: girizzi <girizzi@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 11:45:00 by girizzi           #+#    #+#             */
-/*   Updated: 2026/03/06 11:03:14 by girizzi          ###   ########.fr       */
+/*   Updated: 2026/03/15 18:08:09 by girizzi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 static void	wait_simulation(t_program *program)
 {
 	int	i;
-	int	status;
 
-	i = 0;
-	while (i < program->n_philos)
+	if (program->must_eat != -1)
 	{
-		waitpid(-1, &status, 0);
-		if (WEXITSTATUS(status) == 1)
+		i = 0;
+		while (i < program->n_philos)
 		{
-			cleanup_simulation(program);
-			exit(1);
+			waitpid(-1, NULL, 0);
+			i++;
 		}
-		i++;
 	}
+	else
+		sem_wait(program->dead_lock);
+	cleanup_simulation(program);
 }
 
 int	main(int argc, char **argv)
